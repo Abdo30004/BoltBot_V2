f2785a9b60a2edde9b983d570f0fc03865061ad3
Abderrahmane Yahiaoui <76871067+Abdo30004@users.noreply.github.com>
2023-06-11 20:13:20 +0100

    tk

--- commands/downloaders/tiktok.ts
+++ commands/downloaders/tiktok.ts
@@ -2,6 +2,7 @@ import { getTiktok } from "../../scrappers/tiktok";
 
 import { Command } from "../../interfaces/command";
 import { MessageMedia } from "whatsapp-web.js";
+import axios from "axios";
 const command: Command = {
   name: "tiktok",
   aliases: ["tk"],
@@ -16,11 +17,12 @@ const command: Command = {
       await message.reply(translate.getReply("invalidUrl"));
       return;
     }
-    let media = await MessageMedia.fromUrl(tiktok.link, {
-      filename: "tiktok.mp4",
-        unsafeMime: true,
-       
+    let { data } = await axios.get(tiktok.link, {
+      responseType: "arraybuffer",
     });
+
+    let base64 = Buffer.from(data).toString("base64");
+    let media = new MessageMedia("video/mp4", base64, "tiktok.mp4");
     await message.reply(media);
   },
 };
