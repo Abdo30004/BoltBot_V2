341dfa3fb49a31e7734f6baedaed5adb7e334700
Abderrahmane Yahiaoui <76871067+Abdo30004@users.noreply.github.com>
2023-06-13 19:21:31 +0100

    c

--- commands/public/ping.ts
+++ commands/public/ping.ts
@@ -4,12 +4,15 @@ const command: Command = {
   name: "ping",
   aliases: ["إستجابة"],
   execute: async (client, message, translate, args) => {
-    let ping = Date.now() - message.timestamp * 1000;
-
+    let date = Date.now();
+    let ping = date - message.timestamp * 1000;
+    console.log(message.timestamp);
+    console.log(date);
     await message.reply(
-      `${translate.getReply("pong", [{ key: "ping", value: ping }])}`
+      `${translate.getReply("pong", [
+        { key: "ping", value: Math.floor(ping / 1000) },
+      ])}`
     );
-
   },
 };
 
