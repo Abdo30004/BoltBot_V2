83989bff99f63841c216653b3ff2e7eab1c1d81b
Abderrahmane Yahiaoui <76871067+Abdo30004@users.noreply.github.com>
2023-06-13 01:26:20 +0100

    tiktok

--- commands/downloaders/tiktok.ts
+++ commands/downloaders/tiktok.ts
@@ -3,7 +3,7 @@ import { getTiktok } from "../../scrappers/tiktok";
 import { Command } from "../../interfaces/command";
 import { MessageMedia } from "whatsapp-web.js";
 import download from "download";
-import axios from "axios";
+
 const command: Command = {
   name: "tiktok",
   aliases: ["tk"],
@@ -18,22 +18,38 @@ const command: Command = {
       await message.reply(translate.getReply("invalidUrl"));
       return;
     }
-    if (!tiktok.link) {
-      return;
+    if (tiktok.type == "video") {
+      let video = (await download(tiktok.video)).toString("base64");
+      let media = new MessageMedia("video/mp4", video, "video.mp4");
+      await message.reply(media, null);
     }
-    const buffer = (await download(tiktok.link, {})).toString("base64");
-    const media = new MessageMedia("video/mp4", buffer, "video.mp4");
+    if (tiktok.type == "audio") {
+      let audio = (await download(tiktok.audio)).toString("base64");
+      let media = new MessageMedia("audio/mp3", audio, "audio.mp3");
+      await message.reply(media, null, { sendAudioAsVoice: true });
+    }
+    const isRejected = (
+      input: PromiseSettledResult<unknown>
+    ): input is PromiseRejectedResult => input.status === "rejected";
 
-    /*
-    const { data } = await axios.get(tiktok.link, {
-      responseType: "arraybuffer",
-    });
-    let buffer = Buffer.from(data, "binary").toString("base64");
+    const isFulfilled = <T>(
+      input: PromiseSettledResult<T>
+    ): input is PromiseFulfilledResult<T> => input.status === "fulfilled";
 
-    const media = new MessageMedia("video/mp4", data, "video.mp4");
-*/
-    await message.reply(media, null);
+    if (tiktok.type == "photos") {
+      let photos = (
+        await Promise.allSettled(tiktok.photos.map((photo) => download(photo)))
+      )
+        .filter(isFulfilled)
+        .map((photo) => photo.value.toString("base64"));
+      let medias = photos.map((photo) => {
+        return new MessageMedia("image/jpeg", photo, "photo.jpeg");
+      });
+
+      for (let media of medias) {
+        await message.reply(media, null);
+      }
+    }
   },
 };
-
 export default command;
--- scrappers/tiktok.ts
+++ scrappers/tiktok.ts
@@ -6,14 +6,24 @@ export const getTiktok = async (url: string) => {
     "user-agent":
       "Mozilla/5.0 (X11; Linux x86_64; rv:92.0) Gecko/20100101 Firefox/92.0",
   };
+  let {
+    data: { shortlink: newurl },
+  } = await axios.get(
+    `https://tokurlshortener.com/api/shorten?url=${encodeURI(url)}`,
+    {}
+  );
+  url = newurl;
+
   let { data, headers: resHeaders } = await axios.post(
     "https://musicaldown.com/",
     {
       headers,
     }
   );
+
   let $ = load(data);
   let inputs = $.root().find("input");
+
   let response = {
     data: {
       [inputs.eq(0).attr("name")]: url,
@@ -22,6 +32,10 @@ export const getTiktok = async (url: string) => {
     },
     cookies: resHeaders["set-cookie"][0].split(";"),
   };
+  let form = new URLSearchParams();
+  form.append(inputs.eq(0).attr("name"), url);
+  form.append(inputs.eq(1).attr("name"), inputs.eq(1).attr("value"));
+  form.append(inputs.eq(2).attr("name"), inputs.eq(2).attr("value"));
 
   headers = {
     ...headers,
@@ -45,7 +59,7 @@ export const getTiktok = async (url: string) => {
 
   let { data: tiktokData, request } = await axios.post(
     "https://musicaldown.com/download",
-    new URLSearchParams(response.data),
+    form.toString(),
     {
       headers,
     }
@@ -53,18 +67,35 @@ export const getTiktok = async (url: string) => {
   let $tiktok = load(tiktokData);
   let type = request.res.responseUrl.includes("photo")
     ? "photos"
+    : request.res.responseUrl.includes("mp3")
+    ? "audio"
     : request.res.responseUrl.includes("download")
     ? "video"
     : "error";
 
   if (type === "error") return null;
   if (type === "video") {
+    let { data: audioData } = await axios.post(
+      "https://musicaldown.com/mp3/download/",
+      form.toString(),
+      {
+        headers,
+      }
+    );
+    let $audio = load(audioData);
     return {
       type,
-      link: $tiktok("div[class=row]")
+      video: $tiktok("div[class=row]")
         .eq(1)
         .find(`a[class="btn waves-effect waves-light orange"]`)
         .attr("href"),
+      audio: $audio("source").attr("src"),
+    };
+  }
+  if (type === "audio") {
+    return {
+      type,
+      audio: $tiktok("source").attr("src"),
     };
   }
   if (type === "photos") {
@@ -77,7 +108,9 @@ export const getTiktok = async (url: string) => {
     return {
       type,
       photos,
+      audio: $tiktok(
+        `a[class="btn waves-effect waves-light orange download"]`
+      ).attr("href"),
     };
   }
 };
-
