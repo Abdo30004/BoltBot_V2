d6a5f6e5ca0028209e62b5406721649c14b69a9d
Abderrahmane Yahiaoui <76871067+Abdo30004@users.noreply.github.com>
2023-06-23 16:23:48 +0100

    set flags

--- commands/public/setLanguage.ts
+++ commands/public/setLanguage.ts
@@ -6,15 +6,28 @@ const command: Command = {
   category: "public",
   execute: async (client, message, translate, args) => {
     let lang = args[0];
-    if (!lang) {
-      await message.reply(translate.usage);
-      return false;
-    }
+    let flags = {
+      ar: "🇸🇦",
+      en: "🇺🇸",
+      fr: "🇫🇷",
+      es: "🇪🇸",
+      de: "🇩🇪",
+      it: "🇮🇹",
+      pl: "🇵🇱",
+    };
     let supportedLangs = [...client.i18n.languages.keys()];
-    if (!supportedLangs.includes(lang)) {
+    if (!lang! || supportedLangs.includes(lang)) {
       await message.reply(
         translate.getReply("invalidLanguage", [
-          { key: "langs", value: supportedLangs.join(", ") },
+          {
+            key: "langs",
+            value: `\n${supportedLangs
+              .map(
+                (language) =>
+                  `-${language} ${flags[language as keyof typeof flags]}`
+              )
+              .join("\n")}`,
+          },
         ])
       );
       return false;
@@ -32,15 +45,7 @@ const command: Command = {
       data.settings.language = lang;
     }
     await data.save();
-    let flags = {
-      ar: "🇸🇦",
-      en: "🇺🇸",
-      fr: "🇫🇷",
-      es: "🇪🇸",
-      de: "🇩🇪",
-      it: "🇮🇹",
-      pl: "🇵🇱",
-    };
+
     translate = client.i18n.getCommand(lang, command.name);
     await message.reply(
       translate.getReply("languageChanged", [
