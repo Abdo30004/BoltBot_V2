bb8f3be7378c4fddaf05371069f93459ebb60d55
Abderrahmane Yahiaoui <76871067+Abdo30004@users.noreply.github.com>
2023-06-23 22:57:52 +0100

    fix

--- events/ready.ts
+++ events/ready.ts
@@ -1,24 +1,29 @@
 import Client from "../base/client";
 import { Event } from "../interfaces/event";
+async function updateCache(client: Client) {
+  let chats = await client.getChats();
+  let users = await client.getContacts();
+  let blocks = await client.getBlockedContacts();
+
+  chats.forEach((chat) => {
+    client.cache.chats.set(chat.id._serialized, chat);
+  });
+
+  users.forEach((user) => {
+    client.cache.users.set(user.id._serialized, user);
+  });
+  client.cache.blocks = blocks.map((block) => block.id._serialized);
+}
 
 const event: Event = {
   name: "ready",
   once: false,
   run: async (client: Client) => {
     client.uptime = Date.now();
-
-    let chats = await client.getChats();
-    let users = await client.getContacts();
-    let blocks = await client.getBlockedContacts();
-
-    chats.forEach((chat) => {
-      client.cache.chats.set(chat.id._serialized, chat);
-    });
-
-    users.forEach((user) => {
-      client.cache.users.set(user.id._serialized, user);
-    });
-    client.cache.blocks = blocks.map((block) => block.id._serialized);
+    updateCache(client);
+    setInterval(() => {
+      updateCache(client);
+    }, 1000 * 60 * 60 * 2);
     console.log(`Logged in as ${client.info.pushname}`);
   },
 };
